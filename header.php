495eb91717a84d083be21a2b59e1c05e83387815
Nasem Baki <nbaki@mail.med.upenn.edu>
2013-12-26 16:59:49 -0500

    fixed some error validations

--- header.php
+++ header.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
- <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+ <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" >
 <title><?php wp_title(); ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/style.less" />
+<link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/style.less" >
     <script src="<?php echo get_template_directory_uri(); ?>/js/less.js" type="text/javascript"></script>
     <script type="text/javascript">
         less.modifyVars({
             '@color': '<?php echo heavenly_get_theme_opts('color_scheme','#3399ff'); ?>'
         });
     </script>
-    <script src="<?php echo get_childTheme_url(); ?>/scripts/jquery-1.3.2.min.js"></script>
+    <script src="<?php echo get_childTheme_url(); ?>/scripts/jquery-1.3.2.min.js" type="text/javascript" ></script>
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -51,7 +51,7 @@
 <div class="nav-bar">
 <div class="btn-navbar">
     <a id="mobile-nav-button-minus" href="#" style="display: none;"><img src="http://sourendev.scilsnet.rutgers.edu/wp/wp-content/uploads/2013/12/Minusgreen_button.gif" alt="Mobile Navigation" /></a>
-    <a id="mobile-nav-button-plus" href="#"><img src="http://sourendev.scilsnet.rutgers.edu/wp/wp-content/uploads/2013/12/Plusgreen_button.gif" alt="Mobile Navigation" </a>
+    <a id="mobile-nav-button-plus" href="#" ><img src="http://sourendev.scilsnet.rutgers.edu/wp/wp-content/uploads/2013/12/Plusgreen_button.gif" alt="Mobile Navigation" </a>
 </div>
     <?php
 

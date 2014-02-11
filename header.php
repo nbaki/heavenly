063190a25310cc075c66bf826d03da80a9e0f3a7
Nasem Baki <nbaki22@gmail.com>
2014-02-10 23:49:37 -0500

    Tab style with images

--- header.php
+++ header.php
@@ -22,9 +22,68 @@
         $('.nav:last').append("<li id='last-logo'><img src='<?php echo wp_get_attachment_url(353); ?>' title='Kind Technology Services' alt='Kind Technology Services' width='30' height='50'></li>");
         setHeaderDefaults();
         setMobileNavigation();
-
     });
 </script>
+<?php if (is_page('Home')) : ?>
+    <script>
+        $(document).ready(function () {
+            $('#home_link').css('padding-bottom', 20);
+            $('#home_link').css('border-bottom', 0);
+            $('#home_link').css('border-bottom-left-radius', 0);
+            $('#home_link').css('border-bottom-right-radius', 0);
+            $('#home_link').css('background', '#3FAD21');
+            $('.content-container').css('border-top', '7px solid #3FAD21');
+            $('.content-container').css('border-bottom', '10px solid #3FAD21');        });
+    </script>
+<?php elseif (is_page('About Us')) : ?>
+    <script>
+        $(document).ready(function () {
+            $('#about_link').css('padding-bottom', 20);
+            $('#about_link').css('border-bottom', 0);
+            $('#about_link').css('background', '#3FAD21');
+            $('#about_link').css('border-bottom-left-radius', 0);
+            $('#about_link').css('border-bottom-right-radius', 0);
+            $('.content-container').css('border-top', '7px solid #3FAD21');
+            $('.content-container').css('border-bottom', '10px solid #3FAD21');
+        });
+    </script>
+<?php elseif (is_page('Services')) : ?>
+    <script>
+        $(document).ready(function () {
+            $('#services_link').css('padding-bottom', 20);
+            $('#services_link').css('border-bottom', 0);
+            $('#services_link').css('background', '#3FAD21');
+            $('#services_link').css('border-bottom-left-radius', 0);
+            $('#services_link').css('border-bottom-right-radius', 0);
+            $('.content-container').css('border-top', '7px solid #3FAD21');
+            $('.content-container').css('border-bottom', '10px solid #3FAD21');
+        });
+    </script>
+<?php elseif (is_page('Our Process')) : ?>
+    <script>
+        $(document).ready(function () {
+            $('#processes_link').css('padding-bottom', 20);
+            $('#processes_link').css('border-bottom', 0);
+            $('#processes_link').css('background', '#3FAD21');
+            $('#processes_link').css('border-bottom-left-radius', 0);
+            $('#processes_link').css('border-bottom-right-radius', 0);
+            $('.content-container').css('border-top', '7px solid #3FAD21');
+            $('.content-container').css('border-bottom', '10px solid #3FAD21');
+        });
+    </script>
+<?php elseif (is_page('Contact Us')) : ?>
+    <script>
+        $(document).ready(function () {
+            $('#contact_link').css('padding-bottom', 20);
+            $('#contact_link').css('border-bottom', 0);
+            $('#contact_link').css('background', '#3FAD21');
+            $('#contact_link').css('border-bottom-left-radius', 0);
+            $('#contact_link').css('border-bottom-right-radius', 0);
+            $('.content-container').css('border-top', '7px solid #3FAD21');
+            $('.content-container').css('border-bottom', '10px solid #3FAD21');
+        });
+    </script>
+<?php endif; ?>
  <header id="banner-header">
     <img class="banner" src="<?php echo wp_get_attachment_url(267); ?>" alt="Kind Technology Services" />
  </header>
@@ -42,15 +101,21 @@
     <div class="tab_nav">
 
         <ul class="tabs">
-            <li class="logo"><img src='<?php echo wp_get_attachment_url(353); ?>' title='Kind Technology Services' alt='Kind Technology Services' width='30' height='50' />
-            </li>
-            <li><a class="menu_link" href="/wp/home">Home</a></li>
-            <li><a class="menu_link" href="/wp/about-us">About Us</a></li>
-            <li><a class="menu_link" href="/wp/services">Services</a></li>
-            <li><a class="menu_link" href="/wp/our-process">Our Process</a></li>
-            <li><a class="menu_link" href="/wp/contact-us">Contact Us</a></li>
-            <li class="logo"><img src='<?php echo wp_get_attachment_url(353); ?>' title='Kind Technology Services' alt='Kind Technology Services' width='30' height='50' />
-            </li>
+<!--            <li class="logo"><img src='--><?php //echo wp_get_attachment_url(353); ?><!--' title='Kind Technology Services' alt='Kind Technology Services' width='30' height='50' />-->
+<!--            </li>-->
+            <li>
+                <a class="menu_link" id="home_link" href="/wp/home"><img src="<?php echo get_childTheme_url(); ?>/images/home_button.png" width="20" height="20" style="margin-right: 4px;"/>
+                    Home</a></li>
+            <li><a class="menu_link" id="about_link" href="/wp/about-us">
+                    <img src="<?php echo get_childTheme_url(); ?>/images/about_button.png" width="20" height="20" style="margin-right: 4px;"/>About Us</a></li>
+            <li><a class="menu_link" id="services_link" href="/wp/services">
+                    <img src="<?php echo get_childTheme_url(); ?>/images/wrench_icon.png" width="20" height="20" style="margin-right: 4px;"/>Services</a></li>
+            <li><a class="menu_link" id="processes_link" href="/wp/our-process">
+                    <img src="<?php echo get_childTheme_url(); ?>/images/services_button.png" width="20" height="20" style="margin-right: 4px;"/>Our Process</a></li>
+            <li><a class="menu_link" id="contact_link" href="/wp/contact-us">
+                    <img src="<?php echo get_childTheme_url(); ?>/images/contact_button.png" width="20" height="20" style="margin-right: 4px;"/>Contact Us</a></li>
+<!--            <li class="logo"><img src='--><?php //echo wp_get_attachment_url(353); ?><!--' title='Kind Technology Services' alt='Kind Technology Services' width='30' height='50' />-->
+<!--            </li>-->
         </ul>
     </div>
     <?php
